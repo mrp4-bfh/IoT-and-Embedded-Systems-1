44e56157e6b73a95dfd3f562e1963cb6fe660b94
BTW2402-IoT <62189690+BTW2402-IoT@users.noreply.github.com>
2020-03-15 00:29:22 +0100

    Update

--- Power Management/main.py
+++ Power Management/main.py
@@ -1,12 +1,15 @@
-from machine import Pin, I2C, idle
+from machine import Pin, I2C, idle, RTC, freq
 from time import sleep
 import BME280
+import os
+
+rtc = RTC()
+rtc.datetime((2020, 3, 17, 0, 6, 0, 0, 0))
 
 # ESP32 - Pin assignement
 i2c = I2C(scl=Pin(22), sda=Pin(21), freq=10000)
-# ESP8266 - Pin assignement
-#i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)
 
+# Scan for your BME280 with the i2c.scan() function
 try:
     bme = BME280.BME280(i2c=i2c)
 except OSError as e:
@@ -14,11 +17,22 @@ except OSError as e:
 
 i = 0
 
-while i < 10:
+while i < 20:
+    # Get data from BME280
     temp = bme.temperature
     hum = bme.humidity
     pres = bme.pressure
-    sensor_readings = {'value1':temp, 'value2':hum, 'value3':pres}
+    time = str(rtc.datetime())
+
+    # Build a String
+    sensor_readings = {'temp: '+temp+' humy: '+hum+' presure: '+pres+' log: '+time}
     print(sensor_readings)
-    sleep(1)
+
+
+    # Write so in file
+    f = open('data.txt', 'a')
+    f.write(str(sensor_readings)+'\n')
+    f.close()
+
+    sleep(0.5)
     i += 1
