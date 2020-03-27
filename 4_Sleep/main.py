b3bb421a6411c90a61c2e1a688a5d159edf1f6b1
BTW2402-IoT <62189690+BTW2402-IoT@users.noreply.github.com>
2020-03-27 16:08:33 +0100

    check if the device woke from a deep sleep

--- 4_Sleep/main.py
+++ 4_Sleep/main.py
@@ -1,7 +1,7 @@
 #Task 4.2 Wake up!
 
 import esp32
-from machine import Pin, deepsleep
+from machine import Pin, deepsleep, DEEPSLEEP_RESET, reset_cause
 from time import sleep
 
 led=Pin(2, Pin.OUT)
@@ -12,9 +12,14 @@ button = Pin(14, Pin.IN)
 #level parameter can be: esp32.WAKEUP_ANY_HIGH or esp32.WAKEUP_ALL_LOW
 esp32.wake_on_ext0(pin = button, level = esp32.WAKEUP_ANY_HIGH)
 
+# check if the device woke from a deep sleep
+if reset_cause() == DEEPSLEEP_RESET:
+    print('woke from a deep sleep')
+
 print('start work')
 led.value(1)
-sleep(10)
+sleep(5)
 print('Going to sleep')
 led.value(0)
+#put the device to deepsleep
 deepsleep()
