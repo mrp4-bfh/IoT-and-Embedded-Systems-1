4647be48211c6ea095d28dcb9b7b08c2fb372c61
BTW2402-IoT <62189690+BTW2402-IoT@users.noreply.github.com>
2020-03-27 15:33:48 +0100

    Update
    Solution Task 4.2

---
+++ 4_Sleep/main.py
@@ -0,0 +1,20 @@
+#Task 4.2 Wake up!
+
+import esp32
+from machine import Pin, deepsleep
+from time import sleep
+
+led=Pin(2, Pin.OUT)
+button = Pin(14, Pin.IN)
+
+#Note: with this wake up source, you can only use pins that are RTC GPIOs
+#take a look at our ESP32 GPIO reference guide. 
+#level parameter can be: esp32.WAKEUP_ANY_HIGH or esp32.WAKEUP_ALL_LOW
+esp32.wake_on_ext0(pin = button, level = esp32.WAKEUP_ANY_HIGH)
+
+print('start work')
+led.value(1)
+sleep(10)
+print('Going to sleep')
+led.value(0)
+deepsleep()
